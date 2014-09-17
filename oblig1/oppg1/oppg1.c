41de991335e62d140f12696234f74dcd913a193e
Petter Holt Juliussen <post@petterhj.net>
2014-09-17 13:52:44 -0400

    first

---
+++ oblig1/oppg1/oppg1.c
@@ -0,0 +1,262 @@
+/*
+	INF1060 - Oblig 1 - Oppgave 1
+	Encoding: ISO 8859-1
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+
+
+// Struct (node)
+struct line {
+    char* string;
+    struct line* next;
+};
+
+struct line* first = NULL;
+int lineCount = 0;
+char vowels[] = {'a', 'e', 'i', 'o', 'u', 'y', 'æ', 'ø', 'å'};
+
+// Read file to linked list
+int read_file(char *input_file) {
+    // Open file
+    FILE *f;
+
+    f = fopen(input_file, "r");
+    
+    if (f == NULL) {
+        printf("[ERROR] Could not open file for reading (errno %d: %s)!\n", errno, strerror(errno));
+        return 0;
+    }
+    else {
+        // Generate list
+	    struct line* current = first;
+
+	    char line_buffer[256];
+
+	    while (fgets(line_buffer, sizeof(line_buffer), f)) {
+	    	// Increase line counter
+	    	lineCount++;
+
+	        // Replace any newline with string terminator
+	        if (line_buffer[strlen(line_buffer) - 1] == '\n')           
+	            line_buffer[strlen(line_buffer) - 1] = '\0';
+
+	        // New line node
+	        struct line* newl = (struct line*) malloc(sizeof(struct line));
+	        newl->string = (char*) malloc (strlen(line_buffer) + 1);        // Allocate memory
+	        memcpy(newl->string, line_buffer, strlen(line_buffer) + 1);     // Copy string from buffer to node
+
+	        if (first == NULL)
+	            first = newl;
+	        else
+	            current->next = newl;
+
+	        // Set current to latest line
+	        current = newl;
+	    }
+
+	    // Make sure next of last is NULL
+	    current->next = NULL;
+
+        // Close file
+        fclose(f);
+
+     	return 1;
+    }
+}
+
+// Free memory
+void free_memory(void) {
+	// Free memory
+	struct line* current = first;
+	struct line* next;
+
+	while(current != NULL) {
+    	next = current->next;
+    	free(current->string);  // Free string data
+    	free(current);			// Free node
+    	current = next;			// Repeat until reaching tail
+	}
+}
+
+// Check if character is vowel
+int is_vowel(char c) {
+	int i = 0;
+
+	for (i = 0; i < sizeof(vowels); i++)
+		if (vowels[i] == c)
+			return 1;
+
+	return 0;
+}
+
+// Remove char at index
+void remove_char(char *src, int index) {
+	int i;
+
+    for (i = index; i < strlen(src); i++) {
+       src[i] = src[(i + 1)];
+    }
+}
+
+// [1] Print input file
+void print_file(void) {
+	printf("File contents:\n");
+
+	// Loop list untill next of tail NULL
+    struct line* fline = first;
+    
+    while(fline != NULL) {
+        printf("\t%s\n", fline->string);
+        fline = fline->next;
+    }
+}
+
+// [2] Print random line
+void print_random_line(void) {
+	// Generate random number in range with time as seed, as per
+	//	http://www.cplusplus.com/reference/cstdlib/rand/?kw=rand
+	srand(time(NULL));
+	int r = rand() % lineCount;
+
+	printf("Random line (%d):\n", (r+1));
+	
+	int i = 0;
+	
+    struct line* fline = first;
+    
+    while(fline != NULL) {
+    	if (i == r)
+        	printf("\t%s\n", fline->string);
+        
+        fline = fline->next;
+    	i++;
+    }
+}
+
+// [3] Replace vowels
+void replace_vowels(void) {
+	printf("Replace vowels ...\n");
+
+	int i;
+
+	// Loop through all vowels
+	for (i = 0; i < sizeof(vowels); i++) {
+		printf("\n... with vowel \'%c\'\n", vowels[i]);
+		// Loop lines
+		struct line* fline = first;
+    
+    	while(fline != NULL) {
+        	int j;
+
+        	// Replace vowel
+        	for (j = 0; j < strlen(fline->string); j++)
+        		if (is_vowel(fline->string[j]))
+        			fline->string[j] = vowels[i];
+
+        	// Print line
+        	printf("\t%s\n", fline->string);
+
+        	fline = fline->next;
+    	}
+	}
+}
+
+// [4] Remove vowels
+void remove_vowels(void) {
+	printf("Remove vowels:\n");
+
+	// Loop lines
+	struct line* fline = first;
+
+	while(fline != NULL) {
+    	int i;
+
+    	// Remove vowel
+		for (i = 0; i < strlen(fline->string); i++)
+    		if (is_vowel(fline->string[i]))
+    			remove_char(fline->string, i);
+
+    	// Print line
+    	printf("\t%s\n", fline->string);
+
+    	fline = fline->next;
+	}
+}
+
+// [5] Print number of characters
+void print_character_count(void) {
+	// Loop list and count each line
+	int cc = 0;
+    struct line* fline = first;
+    
+    while(fline != NULL) {
+		cc += strlen(fline->string);
+
+        fline = fline->next;
+    }
+
+    // Sans null chars, and depending on encoding of file
+	printf("The text is %d characters long (sans control chars)\n", cc);
+}
+
+// Print usage
+void print_usage(char *name) {
+	printf("USAGE: %s <command> <input_file>\n\n", name);
+	printf("where <command> is one of the following:\n\n");
+	printf(" print\t\tprint input_file\n");
+	printf(" random\t\tprint a random line\n");
+	printf(" replace\treplace the vowels with all the other vowels\n");
+	printf(" remove\t\tremove vowels\n");
+	printf(" len\t\tprint the number of characters in the input_file\n");
+}
+
+// Main
+int main(int argc, char *argv[]) {
+	// Header
+	printf("\n Oblig 1 - Oppgave 1\n");
+	printf("============================================================\n\n");
+
+	// Check argument count
+	if (argc < 3) {
+		// Usage
+		print_usage(argv[0]);
+	}
+	else {
+		// Options
+		if (strcmp(argv[1], "print") == 0) {
+			if(read_file(argv[2]))
+				print_file();
+		} 
+		else if (strcmp(argv[1], "random") == 0) {
+			if(read_file(argv[2]))
+  				print_random_line();
+		}
+		else if (strcmp(argv[1], "replace") == 0) {
+			if(read_file(argv[2]))
+  				replace_vowels();
+		}
+		else if (strcmp(argv[1], "remove") == 0) {
+			if(read_file(argv[2]))
+  				remove_vowels();
+		}
+		else if (strcmp(argv[1], "len") == 0) {
+			if(read_file(argv[2]))
+  				print_character_count();
+		}
+		else {
+			printf("[ERROR] Invalid command!\n\n");
+			print_usage(argv[0]);
+		}
+
+		// Free allocated memory
+		free_memory();
+	}
+
+	printf("\n");
+
+	return 0;
+}
---
+++ oblig1/oppg2/oppg2.c
@@ -0,0 +1,186 @@
+/*
+	INF1060 - Oblig 1 - Oppgave 2
+	Encoding: ISO 8859-1
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+
+
+unsigned char* buffer = NULL;	// Using character types as numbers
+long fileSize = NULL;
+
+
+// Read file
+int read_file(char *file_name) {
+	// Open file
+	FILE *f;
+
+	f = fopen(file_name, "r");
+	
+	if (f == NULL) {
+		printf("[ERROR] Could not open file for reading (errno %d: %s)!\n", errno, strerror(errno));
+		return 0;
+	}
+	else {
+		// Get size of file
+		fseek(f, 0, SEEK_END);
+		fileSize = ftell(f);
+		rewind(f);
+
+		// Allocate memory
+		buffer = malloc(fileSize);
+
+		// Read file to buffer
+		fread(buffer, fileSize, 1, f);
+	
+		// Close file
+		fclose(f);
+
+		return 1;
+	}
+}
+
+// [1] Print input file
+void print_file() {
+	// Simply print buffer
+	printf("%s", buffer);
+}
+
+// [2] Encode file
+int encode_file(char *output_file) {
+	// Open file for writing
+	FILE *f;
+
+	f = fopen(output_file, "wb");
+	
+	if (f == NULL) {
+		printf("[ERROR] Could not open file for reading (errno %d: %s)!\n", errno, strerror(errno));
+		return 0;
+	}
+	else {
+		// Encode file
+		printf(" Encoding to %s........ ", output_file);
+
+		int i;
+		char ch = 0;
+		int bit = 0;
+
+		// For each char in buffer
+		for(i = 0; i < strlen(buffer); i++) {
+			char hex = 0;
+		
+			if (buffer[i] == ' ')		// ' ' (blank) 		= 00 = 0x0
+				hex = 0x0;
+			if (buffer[i] == ':')		// ':'				= 01 = 0x1
+				hex = 0x1;
+			if (buffer[i] == '@')		// '@'				= 10 = 0x2
+				hex = 0x2;
+			if (buffer[i] == '\n')		// '\n' (newline)	= 11 = 0x3
+				hex = 0x3;
+			
+			// Bit shift left
+			bit = i % 4 + 1; 			// Every fourth
+
+			if (bit == 1)
+				ch |= hex << 6;
+			if (bit == 2)
+				ch |= hex << 4;
+			if (bit == 3)
+				ch |= hex << 2;
+			if (bit == 4) {
+				ch |= hex;
+
+				putc(ch, f);			// Write "generated" char
+
+				ch = 0;
+			}
+		}
+
+		putc('\0', f);
+
+		printf("Done!\n");
+    }
+}
+
+// [3] Decode file
+void decode_file() {
+    char chars[] = {' ', ':', '@', '\n'};
+
+    int i;
+    int bit;
+
+    for(i = 0; i < fileSize; i++) {
+    	// Count to four
+    	for(bit = 1; bit < 5; bit++) {
+            if (bit == 1)
+				printf("%c", chars[buffer[i] >> 6]);
+
+			if (bit == 2)
+        		printf("%c", chars[(buffer[i] >> 4) & 3]);
+
+			if (bit == 3)
+        		printf("%c", chars[(buffer[i] >> 2) & 3]);
+
+        	if (bit == 4)
+        		printf("%c", chars[buffer[i] & 3]);
+        }
+    }
+}
+
+// Print usage
+void print_usage(char *name) {
+	printf("USAGE: %s <command> <input_file> <output_file>\n\n", name);
+	printf("where <command> is one of the following:\n\n");
+	printf(" p\tprint input_file\n");
+	printf("\t(output_file is ignored if specified)\n");
+	printf(" e\tencode/compress input_file to output_file\n");
+	printf(" d\tdecode/uncompress and print input_file\n");
+	printf("\t(output_file is ignored if specified)\n");
+}
+
+// Main
+int main(int argc, char *argv[]) {
+	// Header
+	printf("\n Oblig 1 - Oppgave 2\n");
+	printf("============================================================\n\n");
+
+	// Check argument count
+	if (argc < 3) {
+		// Usage
+		print_usage(argv[0]);
+	}
+	else {
+		// Print
+		if (strcmp(argv[1], "p") == 0) {
+			if(read_file(argv[2]))
+				print_file();
+		}
+		// Encode/Compress
+		else if (strcmp(argv[1], "e") == 0) {
+			if (argc < 4) {
+				printf("[ERROR] No output file specified!\n\n");
+			}
+			else {
+				if(read_file(argv[2]))
+					encode_file(argv[3]);
+			}
+		}
+		// Decode/Uncompress
+		else if (strcmp(argv[1], "d") == 0) {
+			if(read_file(argv[2]))
+				decode_file();
+		}
+		else {
+			printf("[ERROR] Invalid command!\n\n");
+		}
+	}
+
+	// Free allocated buffer memory
+	free(buffer);
+
+	// Footer
+	printf("\n\n");
+}
