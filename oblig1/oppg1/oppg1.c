cde439e9988ab17bd266099381f74d84158dfa2c
Petter Holt Juliussen <post@petterhj.net>
2014-09-17 14:11:57 -0400

    tabs

--- oblig1/oppg1/oppg1.c
+++ oblig1/oppg1/oppg1.c
@@ -11,8 +11,8 @@
 
 // Struct (node)
 struct line {
-    char* string;
-    struct line* next;
+	char* string;
+	struct line* next;
 };
 
 struct line* first = NULL;
@@ -21,51 +21,51 @@ char vowels[] = {'a', 'e', 'i', 'o', 'u', 'y', 'æ', 'ø', 'å'};
 
 // Read file to linked list
 int read_file(char *input_file) {
-    // Open file
-    FILE *f;
+	// Open file
+	FILE *f;
 
-    f = fopen(input_file, "r");
-    
-    if (f == NULL) {
-        printf("[ERROR] Could not open file for reading (errno %d: %s)!\n", errno, strerror(errno));
-        return 0;
-    }
-    else {
-        // Generate list
-	    struct line* current = first;
+	f = fopen(input_file, "r");
+	
+	if (f == NULL) {
+		printf("[ERROR] Could not open file for reading (errno %d: %s)!\n", errno, strerror(errno));
+		return 0;
+	}
+	else {
+		// Generate list
+		struct line* current = first;
 
-	    char line_buffer[256];
+		char line_buffer[256];
 
-	    while (fgets(line_buffer, sizeof(line_buffer), f)) {
-	    	// Increase line counter
-	    	lineCount++;
+		while (fgets(line_buffer, sizeof(line_buffer), f)) {
+			// Increase line counter
+			lineCount++;
 
-	        // Replace any newline with string terminator
-	        if (line_buffer[strlen(line_buffer) - 1] == '\n')           
-	            line_buffer[strlen(line_buffer) - 1] = '\0';
+			// Replace any newline with string terminator
+			if (line_buffer[strlen(line_buffer) - 1] == '\n')           
+				line_buffer[strlen(line_buffer) - 1] = '\0';
 
-	        // New line node
-	        struct line* newl = (struct line*) malloc(sizeof(struct line));
-	        newl->string = (char*) malloc (strlen(line_buffer) + 1);        // Allocate memory
-	        memcpy(newl->string, line_buffer, strlen(line_buffer) + 1);     // Copy string from buffer to node
+			// New line node
+			struct line* newl = (struct line*) malloc(sizeof(struct line));
+			newl->string = (char*) malloc (strlen(line_buffer) + 1);        // Allocate memory
+			memcpy(newl->string, line_buffer, strlen(line_buffer) + 1);     // Copy string from buffer to node
 
-	        if (first == NULL)
-	            first = newl;
-	        else
-	            current->next = newl;
+			if (first == NULL)
+				first = newl;
+			else
+				current->next = newl;
 
-	        // Set current to latest line
-	        current = newl;
-	    }
+			// Set current to latest line
+			current = newl;
+		}
 
-	    // Make sure next of last is NULL
-	    current->next = NULL;
+		// Make sure next of last is NULL
+		current->next = NULL;
 
-        // Close file
-        fclose(f);
+		// Close file
+		fclose(f);
 
-     	return 1;
-    }
+		return 1;
+	}
 }
 
 // Free memory
@@ -75,10 +75,10 @@ void free_memory(void) {
 	struct line* next;
 
 	while(current != NULL) {
-    	next = current->next;
-    	free(current->string);  // Free string data
-    	free(current);			// Free node
-    	current = next;			// Repeat until reaching tail
+		next = current->next;
+		free(current->string);  // Free string data
+		free(current);			// Free node
+		current = next;			// Repeat until reaching tail
 	}
 }
 
@@ -97,9 +97,9 @@ int is_vowel(char c) {
 void remove_char(char *src, int index) {
 	int i;
 
-    for (i = index; i < strlen(src); i++) {
-       src[i] = src[(i + 1)];
-    }
+	for (i = index; i < strlen(src); i++) {
+		src[i] = src[(i + 1)];
+	}
 }
 
 // [1] Print input file
@@ -107,12 +107,12 @@ void print_file(void) {
 	printf("File contents:\n");
 
 	// Loop list untill next of tail NULL
-    struct line* fline = first;
-    
-    while(fline != NULL) {
-        printf("\t%s\n", fline->string);
-        fline = fline->next;
-    }
+	struct line* fline = first;
+	
+	while(fline != NULL) {
+		printf("\t%s\n", fline->string);
+		fline = fline->next;
+	}
 }
 
 // [2] Print random line
@@ -126,15 +126,15 @@ void print_random_line(void) {
 	
 	int i = 0;
 	
-    struct line* fline = first;
-    
-    while(fline != NULL) {
-    	if (i == r)
-        	printf("\t%s\n", fline->string);
-        
-        fline = fline->next;
-    	i++;
-    }
+	struct line* fline = first;
+	
+	while(fline != NULL) {
+		if (i == r)
+			printf("\t%s\n", fline->string);
+		
+		fline = fline->next;
+		i++;
+	}
 }
 
 // [3] Replace vowels
@@ -148,20 +148,20 @@ void replace_vowels(void) {
 		printf("\n... with vowel \'%c\'\n", vowels[i]);
 		// Loop lines
 		struct line* fline = first;
-    
-    	while(fline != NULL) {
-        	int j;
+	
+		while(fline != NULL) {
+			int j;
 
-        	// Replace vowel
-        	for (j = 0; j < strlen(fline->string); j++)
-        		if (is_vowel(fline->string[j]))
-        			fline->string[j] = vowels[i];
+			// Replace vowel
+			for (j = 0; j < strlen(fline->string); j++)
+				if (is_vowel(fline->string[j]))
+					fline->string[j] = vowels[i];
 
-        	// Print line
-        	printf("\t%s\n", fline->string);
+			// Print line
+			printf("\t%s\n", fline->string);
 
-        	fline = fline->next;
-    	}
+			fline = fline->next;
+		}
 	}
 }
 
@@ -173,17 +173,17 @@ void remove_vowels(void) {
 	struct line* fline = first;
 
 	while(fline != NULL) {
-    	int i;
+		int i;
 
-    	// Remove vowel
+		// Remove vowel
 		for (i = 0; i < strlen(fline->string); i++)
-    		if (is_vowel(fline->string[i]))
-    			remove_char(fline->string, i);
+			if (is_vowel(fline->string[i]))
+				remove_char(fline->string, i);
 
-    	// Print line
-    	printf("\t%s\n", fline->string);
+		// Print line
+		printf("\t%s\n", fline->string);
 
-    	fline = fline->next;
+		fline = fline->next;
 	}
 }
 
@@ -191,15 +191,15 @@ void remove_vowels(void) {
 void print_character_count(void) {
 	// Loop list and count each line
 	int cc = 0;
-    struct line* fline = first;
-    
-    while(fline != NULL) {
+	struct line* fline = first;
+	
+	while(fline != NULL) {
 		cc += strlen(fline->string);
 
-        fline = fline->next;
-    }
+		fline = fline->next;
+	}
 
-    // Sans null chars, and depending on encoding of file
+	// Sans null chars, and depending on encoding of file
 	printf("The text is %d characters long (sans control chars)\n", cc);
 }
 
@@ -233,19 +233,19 @@ int main(int argc, char *argv[]) {
 		} 
 		else if (strcmp(argv[1], "random") == 0) {
 			if(read_file(argv[2]))
-  				print_random_line();
+				print_random_line();
 		}
 		else if (strcmp(argv[1], "replace") == 0) {
 			if(read_file(argv[2]))
-  				replace_vowels();
+				replace_vowels();
 		}
 		else if (strcmp(argv[1], "remove") == 0) {
 			if(read_file(argv[2]))
-  				remove_vowels();
+				remove_vowels();
 		}
 		else if (strcmp(argv[1], "len") == 0) {
 			if(read_file(argv[2]))
-  				print_character_count();
+				print_character_count();
 		}
 		else {
 			printf("[ERROR] Invalid command!\n\n");
